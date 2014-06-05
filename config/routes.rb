0c9ac156a9033790684b920c1c6af6dff200ffa0
lutz <lutz.biedinger@gmail.com>
2014-06-05 16:51:44 +0100

    add comment code to blorgh engine

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Blorgh::Engine.routes.draw do
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   root to: "posts#index"
 end
--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140605150631) do
+ActiveRecord::Schema.define(version: 20140605153919) do
+
+  create_table "blorgh_comments", force: true do |t|
+    t.integer  "post_id"
+    t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "blorgh_posts", force: true do |t|
     t.string   "title"
