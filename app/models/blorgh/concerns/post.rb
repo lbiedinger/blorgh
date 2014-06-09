0761fdcc4add27cddb4d88d6a148deb71c4e5059
lutz <lutz.biedinger@gmail.com>
2014-06-09 11:15:42 +0100

    implemented concerns under models/blorgh/concerns/.

---
+++ app/models/blorgh/concerns/post.rb
@@ -0,0 +1,29 @@
+module Blorgh::Concerns::Post
+  extend ActiveSupport::Concern
+
+  # 'included do' causes the included code to be avaluated in the
+  # context where it is included (post.rb), rather than being
+  # executed in the module's ocntext (blorgh/concerns/models/post).
+  included do
+    has_many :comments
+    attr_accessor :author_name
+    belongs_to :author, class_name: Blorgh.author_class.to_s
+
+    before_save :set_author
+
+    private
+      def set_author
+        self.author = Blorgh.author_class.find_or_create_by(name: author_name)
+      end
+  end
+
+  def summary
+    "#{title}"
+  end
+
+  module ClassMethods
+    def some_class_method
+      'some class method string'
+    end
+  end
+end
